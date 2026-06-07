d8341173544841cd9f759c1d51c262c99838b6a4
Anne Broens <anne_broens@hotmail.com>
2026-06-07 19:48:50 +0200

    console

--- Projects/Jammming/src/components/Track/Track.jsx
+++ Projects/Jammming/src/components/Track/Track.jsx
@@ -1,14 +1,20 @@
 function Track ({track, addTrack, removeTrack}) {
+    console.log('preview_url:', track.preview_url);
     return (
-        <div>
-            <p>{track.title}</p>
-            <p>{track.artist}</p>
-            <p>{track.album}</p>
-            {addTrack && <button onClick={() => addTrack(track)}>+</button>}
-            {removeTrack && <button onClick={() => removeTrack(track)}>-</button>}
-
+        <div className="track">
+            <div className="track-info">
+                <p className="track-name">{track.name}</p>
+                <p className="track-artist">{track.artist}</p>
+                <p className="track-album">{track.album}</p>
+            </div>
+            {addTrack && <button className="btn-add" onClick={() => addTrack(track)}>+</button>}
+            {removeTrack && <button className="btn-remove" onClick={() => removeTrack(track)}>-</button>}
+            {track.preview_url && (
+  <audio controls src={track.preview_url} />
+)}
         </div>
     );
 }
 
 export default Track;
+
